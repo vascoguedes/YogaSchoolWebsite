09c4871e02049488063bf76c80d20184e015e352
Vasco Guedes <vasco.guedes8@gmail.com>
2022-03-18 13:53:36 +0000

    update

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -8,7 +8,7 @@ import image from '../images/IMG_1580.JPG'
 function Contacts(params) {
 
 
-    const [option, setOption] = useState(params.option ? params.option : 1);
+    const [option, setOption] = useState(params.option ? params.option : 3);
     console.error(option);
 
     return (
@@ -17,9 +17,9 @@ function Contacts(params) {
 
       <div class='contacts w-100 m-0 pt-5 p-0 d-flex align-items-center'>
 
-        <div class='form col-5 p-5 col-offset-2'>
+        <div class='form col-5 p-5 m-5 col-offset-2'>
           <h1>Contacta-me</h1>
-          <h4>Se tiveres alguma dúvida ou precises de alguma informação, não hesites em mandar-me uma mensagem. <br/>Respondo o mais breve possível.</h4>
+          <h5>Se tiveres alguma dúvida ou precises de alguma informação, não hesites em mandar-me uma mensagem. <br/>Respondo o mais breve possível.</h5>
 
           <options class='d-flex options col-7 p-2 mt-4'>
             <button class={option == 1 ? 'option selected' : 'option'} onClick={() => setOption(1)}>Geral</button>
@@ -49,23 +49,25 @@ function Contacts(params) {
                 </div>
                 <input class='row mt-4 formInput w-100' placeholder='Email'/>
 
-                <select name="Tipo de aula">
-                  <option value="VINYASA YOGA">VINYASA YOGA</option>
-                  <option value="POWER VINYASA YOGA">POWER VINYASA YOGA</option>
-                  <option value="YOGA PARA INICIANTES">YOGA PARA INICIANTES</option>
-                  <option value="YOGA RESTAURATIVO">YOGA RESTAURATIVO</option>
-                </select>
+                <div class='w-100 mt-1'>
+                  <select name="Tipo de aula" class='formInputSelect col-3 m-3'>
+                    <option value="VINYASA YOGA">VINYASA YOGA</option>
+                    <option value="POWER VINYASA YOGA">POWER VINYASA YOGA</option>
+                    <option value="YOGA PARA INICIANTES">YOGA PARA INICIANTES</option>
+                    <option value="YOGA RESTAURATIVO">YOGA RESTAURATIVO</option>
+                  </select>
 
-                <select name="Método de pagamento">
-                  <option value="Dinheiro">Dinheiro</option>
-                  <option value="MB Way">MB Way</option>
-                </select>
+                  <select name="Método de pagamento" class='formInputSelect col-3 m-3'>
+                    <option value="Dinheiro">Dinheiro</option>
+                    <option value="MB Way">MB Way</option>
+                  </select>
 
-                <input type="datetime-local" id="meeting-time"
-                  name="meeting-time" value="2018-06-12T19:30"
-                  min="2018-06-07T00:00" max="2018-06-14T00:00"></input>
+                  <input class='formInputSelect col-3 m-3' type="datetime-local" id="meeting-time"
+                    name="meeting-time" value="2018-06-12T19:30"
+                    min="2018-06-07T00:00" max="2018-06-14T00:00"></input>
+                </div>
 
-                <textarea class='row mt-4 formInput w-100' placeholder='Notas'/>
+                <textarea class='row  formInput w-100' placeholder='Notas'/>
 
               </>
 
@@ -78,31 +80,37 @@ function Contacts(params) {
                 </div>
 
                 <div class='d-flex w-100'>
-                  <input class='row mt-4 formInput col-5' placeholder='Email'/>
-                  <input type="datetime-local" id="meeting-time"
+                  <input class='row formInput col-5' placeholder='Email'/>
+                  <input class='formInputSelect m-4 mb-0' type="datetime-local" id="meeting-time"
                     name="meeting-time" value="2018-06-12T19:30"
                     min="2018-06-07T00:00" max="2018-06-14T00:00"/>
                 </div>
 
-                <select name="Tipo de aula">
-                  <option value="VINYASA YOGA">VINYASA YOGA</option>
-                  <option value="POWER VINYASA YOGA">POWER VINYASA YOGA</option>
-                  <option value="YOGA PARA INICIANTES">YOGA PARA INICIANTES</option>
-                  <option value="YOGA RESTAURATIVO">YOGA RESTAURATIVO</option>
-                </select>
+                <div class='d-flex w-100'>
+                  <input class='row mt-4 formInput col-5' placeholder='Morada'/>
+                  <input class='row mt-4 formInput formInputRight col-5' placeholder='NIF'/>
+                </div>
 
-                <select name="Método de pagamento">
-                  <option value="Dinheiro">Dinheiro</option>
-                  <option value="MB Way">MB Way</option>
-                </select>
+                <div class='w-100 mt-1'>
+                  <select name="Tipo de Plano" class='formInputSelect m-3'>
+                    <option value="VINYASA YOGA">VINYASA YOGA</option>
+                    <option value="POWER VINYASA YOGA">POWER VINYASA YOGA</option>
+                    <option value="YOGA PARA INICIANTES">YOGA PARA INICIANTES</option>
+                    <option value="YOGA RESTAURATIVO">YOGA RESTAURATIVO</option>
+                  </select>
 
+                  <select name="Método de pagamento" class='formInputSelect m-3'>
+                    <option value="Dinheiro">Dinheiro</option>
+                    <option value="MB Way">MB Way</option>
+                  </select>
+                </div>
                 
 
-                <textarea class='row mt-4 formInput w-100' placeholder='Notas'/>                
+                <textarea class='row formInput w-100' placeholder='Notas'/>                
               </>
             }
             
-            <input type="checkbox" name='checkbox' class='formCheckBox mt-4'/>
+            <input type="checkbox" name='checkbox' class='formCheckBox m-3 mt-4'/>
             <label for="checkbox">Desejo receber informações de aulas e eventos futuros</label>
 
             <button class='row mt-3 w-100'>Enviar</button>
