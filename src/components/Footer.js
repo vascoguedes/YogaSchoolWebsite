9f577257a7cef095ab8f5bb4716fe9b0a27f4cf2
Vasco Guedes <up202004395@g.uporto.pt>
2022-08-17 18:07:10 +0100

    footer css fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,17 +8,17 @@ function Footer() {
 
   return (
     <div class="footer">
-      <div class="information container pt-4">
+      <div class="information container p-4">
 
         <div class='row line col-md-4 col-8 w-100' />
         
         <div class='row w-100 d-flex align-items-center justify-content-center'>
           <img class='col-md-2 col-6 p-2' src={logo}/>
 
-          <div class='col-md-6 col-10 rightInfo text-md-end text-center'>
+          <div class='col-md-6 col-10 rightInfo text-md-end text-center pt-2'>
             <p>Praceta Joaquim Fernandes Gomes nº76, Vila Nova de Gaia</p>
             <p>samsarayogastudio@hotmail.com</p>
-            <p>+351 915102157</p>
+            <p className='phone-number'>+351 915102157</p>
             <a href="https://www.instagram.com/samsara.yogastudio/" target="_blank" className='instagram_logo'>Find me on <AiOutlineInstagram/></a>
           </div>
         </div>
